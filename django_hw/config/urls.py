9f637617c9701f9f30a1db2b3c0776800cc4d350
kangjuen <00112233pq@gmail.com>
2024-04-03 13:52:52 +0900

    urls.py에 내용추가

--- django_hw/config/urls.py
+++ django_hw/config/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from homework.views import DoMyHomework
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('domyhomework/', DoMyHomework)
 ]
