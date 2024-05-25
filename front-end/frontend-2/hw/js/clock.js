3b7a2a2def819c3e7e3da634df8d5875427595a3
kangjuen <00112233pq@gmail.com>
2024-05-26 02:20:39 +0900

    날짜추가+섹션구분

--- front-end/frontend-2/hw/js/clock.js
+++ front-end/frontend-2/hw/js/clock.js
@@ -2,10 +2,14 @@ const clock = document.querySelector("h2#clock")
 
 function getClock() {
     const date = new Date();
+    const year = String(date.getFullYear());
+    const month = String(date.getMonth()+1).padStart(2, "0");
+    const today = String(date.getDate()).padStart(2, "0");
     const hours = String(date.getHours()).padStart(2, "0");
     const minutes = String(date.getMinutes()).padStart(2, "0");
     const seconds = String(date.getSeconds()).padStart(2, "0");
-    clock.innerHTML = `${hours}:${minutes}:${seconds}`;
+    clock.innerHTML = `오늘은 ${year}년 ${month}월 ${today}일이며,
+    현재 시각은 ${hours}:${minutes}:${seconds}입니다.`;
 }
 
 getClock();
