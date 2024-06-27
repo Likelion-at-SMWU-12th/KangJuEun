408c8996b2c2159df58a39d1e954e7886d73c045
kangjuen <00112233pq@gmail.com>
2024-06-28 01:31:25 +0900

    컴포넌트 사용

---
+++ front-end/frontend-3/review/review/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Hello from "./Hello";
+
+function App() {
+  return (
+    <div>
+      <h1> 복습을 해보자 ~ </h1>
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
---
+++ front-end/frontend-3/review/review/src/Hello.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Hello = () => {
+  return <div>요즘 밤에는 별로 안더운 듯</div>;
+};
+
+export default Hello;
