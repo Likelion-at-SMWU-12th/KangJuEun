6fd15f2a066af8a968f3821ebb4e7edc3158de30
kangjuen <00112233pq@gmail.com>
2024-06-29 21:50:41 +0900

    복습 끝

--- front-end/frontend-3/review/review/src/App.js
+++ front-end/frontend-3/review/review/src/App.js
@@ -3,11 +3,13 @@ import ExternalButton from "./ExternalButton";
 import Hello from "./Hello";
 import InlineButton from "./InlineButton";
 import LikeButton from "./LikeButton";
+import FunnyButton from "./FunnyButton";
 
 function App() {
   return (
     <div>
       <LikeButton />
+      <FunnyButton />
       {/*<h1> 복습을 해보자 ~ </h1>
       <Hello />
       <InlineButton />
