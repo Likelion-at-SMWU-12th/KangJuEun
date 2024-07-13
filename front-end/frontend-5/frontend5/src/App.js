99b9d98dd243aa65a22108e806439875fcb7072d
kangjuen <00112233pq@gmail.com>
2024-07-13 19:22:58 +0900

    useCallback

--- front-end/frontend-5/frontend5/src/App.js
+++ front-end/frontend-5/frontend5/src/App.js
@@ -1,8 +1,94 @@
-import "./App.css";
-import InputSample from "./InputSample";
+import React, { useCallback, useState, useRef } from "react";
+import UserList from "./UserList";
+import CreateUser from "./CreateUser";
 
 function App() {
-  return <InputSample />;
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+  });
+
+  const { username, email } = inputs;
+
+  const onChange = useCallback(
+    (e) => {
+      const { value, name } = e.target;
+      setInputs({
+        ...inputs,
+        [name]: value,
+      });
+    },
+    [inputs]
+  );
+
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: "mandoo",
+      email: "mdjoa@e.com",
+      active: true,
+    },
+    {
+      id: 2,
+      username: "hi",
+      email: "ppp@e.com",
+      active: false,
+    },
+    {
+      id: 3,
+      username: "good",
+      email: "gd@e.com",
+      active: false,
+    },
+  ]);
+
+  const nextId = useRef(4);
+  const onCreate = useCallback(() => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    setUsers(users.concat(user));
+
+    setInputs({
+      username: "",
+      email: "",
+    });
+    nextId.current += 1;
+  }, [users]);
+
+  const onRemove = useCallback(
+    (id) => {
+      setUsers(users.filter((user) => user.id !== id));
+    },
+    [users]
+  );
+
+  const onToggle = useCallback(
+    (id) => {
+      setUsers(
+        users.map((user) =>
+          user.id === id ? { ...users, active: !user.active } : user
+        )
+      );
+    },
+    [users]
+  );
+
+  return (
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <br />
+      <hr />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+    </>
+  );
 }
 
 export default App;
