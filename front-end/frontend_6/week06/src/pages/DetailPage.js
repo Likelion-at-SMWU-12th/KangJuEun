4116541d820a873d4730573cb3e26be1ab12efbc
kangjuen <00112233pq@gmail.com>
2024-07-22 14:57:55 +0900

    6주차 커밋

---
+++ front-end/frontend_6/week06/src/pages/DetailPage.js
@@ -0,0 +1,102 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import Button from "../components/Button";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
+
+const DetailPage = () => {
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const [detail, setDetail] = useState([]);
+
+  const getDetail = () => {
+    axios
+      .get(`http://127.0.0.1:8000/entries/${id}/`)
+      .then((response) => {
+        setDetail(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    getDetail();
+  }, []);
+
+  const deleteComment = () => {
+    axios
+      .delete(`http://127.0.0.1:8000/entries/${id}/`)
+      .then(() => {
+        alert("삭제되었습니다!");
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("삭제 중 오류 발생.");
+      });
+  };
+
+  const editComment = () => {
+    navigate(`/edit/${id}`);
+  };
+
+  return (
+    <Wrapper>
+      <Button txt={"방명록 작성하기"} onBtnClick={() => navigate("/write")} />
+      <DetailWrapper>
+        <DetailDiv>
+          <Author>{detail.author}</Author>
+          <Time>{detail.timestamp}</Time>
+          <Comment>{detail.comment}</Comment>
+          <BtnLine>
+            <Button txt={"수정"} fontSize={"30px"} onBtnClick={editComment} />
+            <Button txt={"삭제"} fontSize={"30px"} onBtnClick={deleteComment} />
+          </BtnLine>
+        </DetailDiv>
+      </DetailWrapper>
+    </Wrapper>
+  );
+};
+
+export default DetailPage;
+
+const Wrapper = styled.div`
+  margin-top: 20px;
+`;
+const DetailWrapper = styled.div`
+  width: calc(100% - 200px);
+  height: fit-content;
+  background-color: white;
+  border-radius: 20px;
+  padding: 100px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+  margin-top: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+`;
+const DetailDiv = styled.div``;
+const Author = styled.div`
+  font-size: 50px;
+  font-weight: 700;
+`;
+const Time = styled.div`
+  color: #c8c8c8;
+  font-weight: 600;
+  font-size: 30px;
+`;
+const Comment = styled.div`
+  font-size: 40px;
+  font-weight: 700;
+  margin: 50px 0;
+`;
+const BtnLine = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0 20px;
+`;
+
+//커밋을 위한 주석추가
---
+++ front-end/frontend_6/week06/src/pages/EditPage.js
@@ -0,0 +1,126 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import Button from "../components/Button";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+
+const EditPage = () => {
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const [author, setAuthor] = useState("");
+  const [comment, setComment] = useState("");
+
+  const onChangeAuthor = (e) => {
+    setAuthor(e.target.value);
+  };
+  const onChangeComment = (e) => {
+    setComment(e.target.value);
+  };
+
+  const getDetail = () => {
+    axios
+      .get(`http://127.0.0.1:8000/entries/${id}/`)
+      .then((response) => {
+        console.log(response);
+        setAuthor(response.data.author);
+        setComment(response.data.comment);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    getDetail();
+  }, []);
+
+  const editComment = () => {
+    axios
+      .put(`http://127.0.0.1:8000/entries/${id}/`, {
+        author: author,
+        comment: comment,
+      })
+      .then((response) => {
+        console.log(response);
+        alert("수정되었습니다!");
+        setAuthor(response.data.author);
+        setComment(response.data.comment);
+        navigate(`/comment/${id}`);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <Wrapper>
+      <InputTitle>이름</InputTitle>
+      <StyledInput
+        placeholder="이름을 입력해주세요."
+        value={author}
+        onChange={onChangeAuthor}
+      />
+      <InputTitle>내용</InputTitle>
+      <StyledTxtarea
+        placeholder="방명록 내용을 입력해주세요."
+        value={comment}
+        onChange={onChangeComment}
+      />
+      <BtnDiv>
+        <Button txt={"수정하기"} onBtnClick={editComment} />
+      </BtnDiv>
+    </Wrapper>
+  );
+};
+export default EditPage;
+
+const Wrapper = styled.div`
+  margin-top: 50px;
+  //padding: 0 100px 0 0;
+`;
+const InputTitle = styled.div`
+  font-size: 30px;
+  font-weight: 700;
+`;
+const StyledInput = styled.input`
+  border: none;
+  outline: none;
+  background-color: white;
+  padding: 10px 30px;
+  border-radius: 15px;
+  width: 300px;
+  height: 50px;
+  font-size: 18px;
+  font-weight: 700;
+  margin: 10px 0 30px 0;
+  &::placeholder {
+    color: #acacac;
+    font-weight: 700;
+  }
+`;
+const StyledTxtarea = styled.textarea`
+  border: none;
+  outline: none;
+  width: calc(100% - 60px);
+  height: 200px;
+  background-color: white;
+  padding: 30px;
+  border-radius: 15px;
+  font-size: 18px;
+  font-weight: 700;
+  margin-top: 10px;
+  resize: none;
+  &::placeholder {
+    color: #acacac;
+    font-weight: 700;
+  }
+`;
+const BtnDiv = styled.div`
+  margin-top: 50px;
+  display: flex;
+  > Button {
+    margin-left: auto;
+  }
+`;
+
+//커밋을 위한 주석추가
