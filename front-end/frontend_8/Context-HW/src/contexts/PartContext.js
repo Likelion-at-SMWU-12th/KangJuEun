ffcdafecfbafda8f27ed35fc1bc446342f542c16
kangjuen <00112233pq@gmail.com>
2024-09-08 19:52:36 +0900

    [Add] Provider, 커스텀 훅 설정

--- front-end/frontend_8/Context-HW/src/contexts/PartContext.js
+++ front-end/frontend_8/Context-HW/src/contexts/PartContext.js
@@ -1,5 +1,18 @@
-import { createContext } from "react";
+import { createContext, useContext, useState } from "react";
 
+// 1. context 생성
 const PartContext = createContext();
 
-export default PartContext;
+export const PartProvider = ({ children }) => {
+  const [part, setPart] = useState("");
+
+  // 2. provider 컴포넌트
+  return (
+    <PartContext.Provider value={{ part, setPart }}>
+      {children}
+    </PartContext.Provider>
+  );
+};
+
+// 3. usePart 커스텀 훅
+export const usePart = () => useContext(PartContext);
--- front-end/frontend_8/Context-HW/src/index.js
+++ front-end/frontend_8/Context-HW/src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import PartContext from "./contexts/PartContext";
+import { PartProvider } from "./contexts/PartContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <PartContext.Provider value={part}>
+    <PartProvider>
       <App />
-    </PartContext.Provider>
+    </PartProvider>
   </React.StrictMode>
 );
