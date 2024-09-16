6f87e5074dcc311b864144a7a63c95cd691cc1c0
kangjuen <00112233pq@gmail.com>
2024-09-16 11:49:38 +0900

    [Add] Store.js 작성

---
+++ front-end/frontend-9-1/zustand-hw/src/store/store.js
@@ -0,0 +1,40 @@
+import { create } from "zustand";
+import { devtools } from "zustand/middleware";
+
+const useQuoteStore = create(
+  devtools((set) => ({
+    quotes: [],
+    addQuote: (quote, source) =>
+      set((state) => ({
+        quotes: [
+          ...state.quotes,
+          { id: Date.now(), quote, source, starred: false },
+        ],
+      })),
+
+    removeQuote: (id) =>
+      set((state) => ({
+        quotes: state.quotes.filter((quote) => quote.id !== id),
+      })),
+
+    // 체크박스 대신 별표(⭐️)
+    toggleStar: (id) =>
+      set((state) => ({
+        quotes: state.quotes.map((quote) =>
+          quote.id === id ? { ...quote, starred: !quote.starred } : quote
+        ),
+      })),
+
+    // 수정 기능 추가!
+    updateQuote: (id, newQuote, newSource) =>
+      set((state) => ({
+        quotes: state.quotes.map((quote) =>
+          quote.id === id
+            ? { ...quote, quote: newQuote, source: newSource }
+            : quote
+        ),
+      })),
+  }))
+);
+
+export default useQuoteStore;
