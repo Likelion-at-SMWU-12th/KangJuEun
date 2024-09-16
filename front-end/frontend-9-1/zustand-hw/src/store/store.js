dbbc44549f5b926302ff96332d8114b837a55d54
kangjuen <00112233pq@gmail.com>
2024-09-16 12:36:07 +0900

    [Add] store.js 플레이리스트 기능 구현, 더미데이터

--- front-end/frontend-9-1/zustand-hw/src/store/store.js
+++ front-end/frontend-9-1/zustand-hw/src/store/store.js
@@ -1,30 +1,90 @@
 import { create } from "zustand";
-import { devtools } from "zustand/middleware";
 
-const useQuoteStore = create(
-  devtools((set) => ({
-    quotes: [],
-    addQuote: (quote, source) =>
-      set((state) => ({
-        quotes: [
-          ...state.quotes,
-          { id: Date.now(), quote, source, starred: false },
-        ],
-      })),
+const useMusicStore = create((set) => ({
+  songs: [
+    {
+      id: 1,
+      title: "Bohemian Rhapsody",
+      artist: "Queen",
+      albumCover: "/images/bohemian.jpg",
+      length: "5:55",
+    },
+    {
+      id: 2,
+      title: "Don't Stop Me Now",
+      artist: "Queen",
+      albumCover: "/images/dont.jpg",
+      length: "3:29",
+    },
+    {
+      id: 3,
+      title: "Somebody to love",
+      artist: "Queen",
+      albumCover: "/images/somebody.jpg",
+      length: "4:57",
+    },
+    {
+      id: 4,
+      title: "Sweet Child O' Mine",
+      artist: "Guns N' Roses",
+      albumCover: "/images/sweet.jpg",
+      length: "5:56",
+    },
+    {
+      id: 5,
+      title: "November Rain",
+      artist: "Guns N' Roses",
+      albumCover: "/images/november.jpg",
+      length: "8:57",
+    },
+    {
+      id: 6,
+      title: "Welcome to the Jungle",
+      artist: "Guns N' Roses",
+      albumCover: "/images/Welcomtothejungle.jpg",
+      length: "4:31",
+    },
+    {
+      id: 7,
+      title: "Seasons",
+      artist: "Wave to Earth",
+      albumCover: "/images/seasons.jpg",
+      length: "4:02",
+    },
+    {
+      id: 8,
+      title: "Light",
+      artist: "Wave to Earth",
+      albumCover: "/images/light.jpg",
+      length: "3:46",
+    },
+    {
+      id: 9,
+      title: "Bad",
+      artist: "Wave to Earth",
+      albumCover: "/images/bad.jpg",
+      length: "4:24",
+    },
+  ],
 
-    removeQuote: (id) =>
-      set((state) => ({
-        quotes: state.quotes.filter((quote) => quote.id !== id),
-      })),
+  // 배열을 순회하면서, hearted(마음꾹)된 id를 찾고 이를 상태에 반영
+  playlist: [],
+  toggleHeart: (id) =>
+    set((state) => ({
+      songs: state.songs.map((song) =>
+        song.id === id ? { ...song, hearted: !song.hearted } : song
+      ),
+    })),
 
-    // 체크박스 대신 별표(⭐️)
-    toggleStar: (id) =>
-      set((state) => ({
-        quotes: state.quotes.map((quote) =>
-          quote.id === id ? { ...quote, starred: !quote.starred } : quote
-        ),
-      })),
-  }))
-);
+  // playlist 배열에 추가할 노래를 id로 찾고,
+  // 동일한 곡이 추가시 ...song을 통해 복사되어 각각의 곡이 개별적으로 추가됨
+  addToPlaylist: (id) =>
+    set((state) => {
+      const song = state.songs.find((song) => song.id === id);
+      if (song) {
+        return { playlist: [...state.playlist, { ...song }] };
+      }
+    }),
+}));
 
-export default useQuoteStore;
+export default useMusicStore;
